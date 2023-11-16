9533b375e82b8331f23c97896bffc5c85eea4c26
Patrick Hammer <patham9@gmail.com>
2023-11-16 11:29:08 +0100

    Update: removal of unnecessary files

--- build.sh
+++ build.sh
@@ -2,8 +2,7 @@ rm -rf ./OpenNARS-for-Applications
 git clone https://github.com/opennars/OpenNARS-for-Applications
 cd ./OpenNARS-for-Applications/
 git checkout portability
-#cp ../NAL.h ./src/
-#cp ../Config.h ./src/
+cp ../Config.h ./src/
 sh build_browser.sh
 sh build.sh
 cp NAR.wasm ../
