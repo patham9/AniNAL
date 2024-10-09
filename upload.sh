22454a2b2b7b50af7f26ee7e82c0fab323e00aec
Patrick Hammer <patham9@gmail.com>
2024-10-09 09:22:43 +0200

    Update: func equ disabled by default

---
+++ upload.sh
@@ -0,0 +1,1 @@
+scp NAR.js demo_complex.html demo_complex_shades.html NAR.wasm demo_complex_continuous_shades.html demo.html  demo_complex_continuous_verbal.html options.js root@91.203.212.130:/var/www/html/AniNAL/
